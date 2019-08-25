bf7d343ff4def5b25a84e23d22cbbfd17ebb0712
peeweek <peeweek@gmail.com>
2019-08-25 11:09:55 +0200

    Final Updates for 2019.2

--- Assets/Script/ConsoleCommnands/LoadLevelConsoleCommand.cs
+++ Assets/Script/ConsoleCommnands/LoadLevelConsoleCommand.cs
@@ -1,37 +1,39 @@
-using Console;
 using GameplayIngredients;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[AutoRegisterConsoleCommand]
-public class LoadLevelConsoleCommand : IConsoleCommand
+namespace ConsoleUtility
 {
-    public string name => "load";
-
-    public string summary => "Loads a level";
-
-    public string help => "load <level-Id>";
-
-    public IEnumerable<Console.Console.Alias> aliases
+    [AutoRegisterConsoleCommand]
+    public class LoadLevelConsoleCommand : IConsoleCommand
     {
-        get
-        {
-            yield return new Console.Console.Alias() { AliasString = "mainmenu", Command = "load -1"  } ;
-        }
-    }
+        public string name => "load";
 
-    public void Execute(string[] args)
-    {
-        if(args.Length == 1)
+        public string summary => "Loads a level";
+
+        public string help => "load <level-Id>";
+
+        public IEnumerable<Console.Alias> aliases
         {
-            int idx;
-            if(int.TryParse(args[0], out idx))
+            get
             {
-                var manager = Manager.Get<GameManager>();
-                idx = Mathf.Clamp(idx, -1, manager.MainGameLevels.Length-1);
-                Console.Console.Log(name, $"Loading Game level #{idx} ...");
-                manager.SwitchLevel(idx, true, null);
+                yield return new Console.Alias() { AliasString = "mainmenu", Command = "load -1"  } ;
+            }
+        }
+
+        public void Execute(string[] args)
+        {
+            if(args.Length == 1)
+            {
+                int idx;
+                if(int.TryParse(args[0], out idx))
+                {
+                    var manager = Manager.Get<GameManager>();
+                    idx = Mathf.Clamp(idx, -1, manager.MainGameLevels.Length-1);
+                    Console.Log(name, $"Loading Game level #{idx} ...");
+                    manager.SwitchLevel(idx, true, null);
+                }
             }
         }
     }
